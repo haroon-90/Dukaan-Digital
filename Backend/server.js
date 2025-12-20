e37605be7fb8f81012dd6a4c8db5258acf779848
haroon-90 <haroonboy90@gmail.com>
2025-12-20 16:51:04 +0500

    17

--- Backend/server.js
+++ Backend/server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: ['https://haroon-90.github.io', 'http://localhost:5173', 'http://dukaan-digital-view.vercel.app'],
+  origin: ['https://haroon-90.github.io', 'http://localhost:5173', 'http://dukaan-digital.vercel.app'],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
